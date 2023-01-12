d2f6e3a11e313330027cf5d0fa2e579083dc2976
Rama Krishna Thotapally <rama.thotapally@ATMECS.CORP>
2023-01-12 11:43:49 +0530

    homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmployeeDataService } from '../services/employee-data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  emplist: any =[];
+  constructor(private empData: EmployeeDataService) { }
   ngOnInit(): void {
+    this.empData.employees().subscribe((data) => {
+      this.emplist=data;
+      console.log(this.emplist);
+    });
   }
-
+  addNewEmployee()
+  {
+    
+  }
 }
